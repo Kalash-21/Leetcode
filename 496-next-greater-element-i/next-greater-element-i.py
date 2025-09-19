07c37a8ef566a0c14804a4d89101073fc1a2bfce
Kalash-21 <68323251+Kalash-21@users.noreply.github.com>
2025-09-18 21:46:09 -0400

    Time: 4 ms (32.62%) | Memory: 17.9 MB (89.60%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,19 +1,36 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        a=[]
-        for char in nums1:
-            pointer=0
-            for i in range(len(nums2)):
-                if char == nums2[i]:
-                    pointer=i+1
-            while pointer<len(nums2):
-                if char< nums2[pointer]:
-                    a.append(nums2[pointer])
-                    break
-                else:
-                    pointer+=1
-            else :
-                a.append(-1)
-        return a
+        nums1Idx = {n:i for i, n in enumerate(nums1)}
+        res = [-1] * len(nums1)
+
+        stack = []
+        for i in range(len(nums2)):
+            cur = nums2[i]
+            # while stack not empty and current > last element in stack
+            while stack and cur > stack[-1]:
+                val = stack.pop()
+                idx = nums1Idx[val]
+                res[idx] = cur
+            # push only if it belongs to nums1
+            if cur in nums1Idx:
+                stack.append(cur)
+
+        return res
+        # Solution : Brute Force
+        # a=[]
+        # for char in nums1:
+        #     pointer=0
+        #     for i in range(len(nums2)):
+        #         if char == nums2[i]:
+        #             pointer=i+1
+        #     while pointer<len(nums2):
+        #         if char< nums2[pointer]:
+        #             a.append(nums2[pointer])
+        #             break
+        #         else:
+        #             pointer+=1
+        #     else :
+        #         a.append(-1)
+        # return a
                     
         
