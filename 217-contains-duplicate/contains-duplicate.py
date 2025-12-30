0d2de8ad44221af375736dfe9019655fd61a8058
Kalash-21 <68323251+Kalash-21@users.noreply.github.com>
2025-12-29 20:45:04 -0500

    Time: 11 ms (74.76%) | Memory: 29.1 MB (80.86%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.py
@@ -0,0 +1,11 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        a=set()
+        for num in nums:
+            if num in a:
+                return True
+            else:
+                a.add(num)
+        return False
+                 
+        
