ed4a4c0fb878c8ec485a451d707b9fa0226456e1
Kalash-21 <68323251+Kalash-21@users.noreply.github.com>
2026-04-28 17:39:23 -0400

    Time: 157 ms (5.07%) | Memory: 19.5 MB (80.30%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -1,22 +1,42 @@
 class Solution:
-    def validPalindrome(self, s: str) -> bool:
-        
-        def checkPalindrome(left, right):
-            while left < right:
-                if s[left] != s[right]:
-                    return False
-                left += 1
-                right -= 1
-            return True
-
-        l = 0
-        r = len(s) - 1
-
-        while l < r:
-            if s[l] == s[r]:
-                l += 1
-                r -= 1
+    def checkagain(self,s,l,r):
+        while l<r:
+            # is alpha numeric
+            if s[l].isalnum():
+                if s[r].isalnum():
+                    if s[l].lower()!=s[r].lower():
+                        return False
+                else:
+                    r-=1
+                    continue
             else:
-                return checkPalindrome(l + 1, r) or checkPalindrome(l, r - 1)
-
+                l+=1
+                continue
+            l+=1
+            r-=1
         return True
+    def validPalindrome(self, s: str) -> bool:
+        l=0
+        r=len(s)-1
+        
+        while l<r:
+            # is alpha numeric
+            if s[l].isalnum():
+                if s[r].isalnum():
+                    if s[l].lower()!=s[r].lower():
+                        return self.checkagain(s,l+1,r) or self.checkagain(s,l,r-1)
+                else:
+                    r-=1
+                    continue
+            else:
+                l+=1
+                continue
+            l+=1
+            r-=1
+
+        return True 
+
+
+        
+
+        
