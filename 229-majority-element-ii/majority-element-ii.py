98309bddbade02cb93ffd95a7ba2df4b7246ff9b
Kalash-21 <68323251+Kalash-21@users.noreply.github.com>
2026-06-09 16:19:31 -0400

    Time: 11 ms (35.58%) | Memory: 21.9 MB (82.97%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -6,26 +6,19 @@ class Solution:
         count2=0
         result=[]
         for num in nums:
-            if candidateName1==None :
+            if num == candidateName1:
+                count1 += 1
+            elif num == candidateName2:
+                count2 += 1
+            elif count1 == 0:
                 candidateName1 = num
-                pass
-            if candidateName2==None:
-                candidateName2= num
-                pass
-            if candidateName1==num:
-                count1+=1
-            elif candidateName2==num:
-                count2+=1
+                count1 = 1
+            elif count2 == 0:
+                candidateName2 = num
+                count2 = 1
             else:
-                if count1 and count2:
-                    count1-=1
-                    count2-=1
-                elif count1==0:
-                    candidateName1 = num
-                    count1=1
-                elif count2==0:
-                    candidateName2 = num
-                    count2=1
+                count1 -= 1
+                count2 -= 1
         count=0
         for num in nums:
             if num==candidateName1:
@@ -41,3 +34,5 @@ class Solution:
         if count>len(nums)/3:
             result.append(candidateName2)
         return result
+
+            
