2e3af94c888ad679d153b1ccce25ecebb0dbd82b
Kalash-21 <68323251+Kalash-21@users.noreply.github.com>
2026-06-10 15:54:15 -0400

    Time: 59 ms (50.15%) | Memory: 19.5 MB (43.05%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -1,42 +1,21 @@
 class Solution:
-    def checkagain(self,s,l,r):
-        while l<r:
-            # is alpha numeric
-            if s[l].isalnum():
-                if s[r].isalnum():
-                    if s[l].lower()!=s[r].lower():
-                        return False
-                else:
-                    r-=1
-                    continue
-            else:
+    def checkAgain(self,s,l,r):
+        while l < r:
+            if s[l]==s[r]:
+                r-=1
                 l+=1
-                continue
-            l+=1
-            r-=1
+            else:
+                return False
         return True
     def validPalindrome(self, s: str) -> bool:
         l=0
         r=len(s)-1
-        
-        while l<r:
-            # is alpha numeric
-            if s[l].isalnum():
-                if s[r].isalnum():
-                    if s[l].lower()!=s[r].lower():
-                        return self.checkagain(s,l+1,r) or self.checkagain(s,l,r-1)
-                else:
-                    r-=1
-                    continue
-            else:
+        while l < r:
+            if s[l]==s[r]:
+                r-=1
                 l+=1
-                continue
-            l+=1
-            r-=1
-
-        return True 
-
-
-        
-
+            else:
+                return self.checkAgain(s,l+1,r) or self.checkAgain(s,l,r-1)
+        return True
+            
         
